bc4c9c02e60e63cc8e4a02077dca95e6ed4075e4
Simon Dulovec <simondulovec@outlook.com>
2020-11-04 13:28:22 +0100

    Zakladna MVC aplikacia

---
+++ InzeratnyPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InzeratnyPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
